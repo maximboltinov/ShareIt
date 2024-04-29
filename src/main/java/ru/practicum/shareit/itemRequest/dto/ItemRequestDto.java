dba0dbd2d627ec1e433bf796ac62a5c22a65329f
Boltinov <maxim.boltinov@yandex.com>
2024-04-29 14:26:33 +0300

    WebMvcTest тесты для ItemRequestController

--- src/main/java/ru/practicum/shareit/itemRequest/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/itemRequest/dto/ItemRequestDto.java
@@ -1,11 +1,15 @@
 package ru.practicum.shareit.itemRequest.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
+@EqualsAndHashCode
 public class ItemRequestDto {
     @NotBlank
     @Length(max = 512)
